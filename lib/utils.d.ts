3cd50a91cc49318d2b564ee8468f94eaf10d2e66
Ole Kristensen <ole@kristensen.name>
2025-04-28 07:52:02 +0000

    fix commit build

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,6 +1,6 @@
 export declare function getBaseLog(x: number, y: number): number;
 export declare function dbFromFloat(floatVal: number): number;
 export declare function findAudioProcBufferSize(numSamplesIn: number): number;
-export declare function dbTicks(min: number, max: number, tickSize: number): number[];
+export declare function dbTicks(min: number, max: number, tickSize: number, scale: string): number[];
 export declare function testSignalGenerator(hz: number, rotation?: number, sampleRate?: number): number[];
 export declare function offsetSincGenerator(): number[];
